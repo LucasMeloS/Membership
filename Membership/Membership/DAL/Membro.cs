782d9d39894f5730beef90a0fad4ef67a271d439
Gabriel Adam <gabrieladam@hotmail.com>
2018-07-03 08:52:19 -0300

    Criando A pasta DAL

--- Membership/Membership/DAL/Membro.cs
+++ Membership/Membership/DAL/Membro.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Membership
+namespace Membership.DAL
 {
     using System;
     using System.Collections.Generic;
